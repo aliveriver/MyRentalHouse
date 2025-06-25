8b003e45177c9324e3e61276747eca684f34f9c3
查无此人 <woshi123qaq@163.com>
2025-06-25 13:01:53 +0800

    完善layout

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,39 +1,48 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header class="header">
-        <h2>微商城后台管理系统</h2>
-        <el-dropdown v-if="store.isLogin">
-          <div>
-            <el-avatar
-              :size="44"
-              src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
-            />
+      <el-aside class="aside">
+        <el-menu
+          class="menu"
+          background-color="#181818"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <div class="header-left">
+            <el-icon><Menu /></el-icon>
+            <h2>二手房管理平台</h2>
           </div>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item @click="logout">退出</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-        <router-link to="/login" v-else class="toLogin">登录</router-link>
-      </el-header>
+          <router-link
+            v-for="(item, index) in constantRoutes"
+            :to="item.path"
+            :key="index"
+          >
+            <el-menu-item :index="index">
+              <el-icon>
+                <component :is="item.meta.icon"></component>
+              </el-icon>
+              <span>{{ item.meta.title }}</span>
+            </el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-aside>
       <el-container>
-        <el-aside class="aside">
-          <el-menu class="menu">
-            <router-link
-              v-for="(item, index) in constantRoutes"
-              :to="item.path"
-            >
-              <el-menu-item :index="index">
-                <el-icon>
-                  <component :is="item.meta.icon"></component>
-                </el-icon>
-                <span>{{ item.meta.title }}</span>
-              </el-menu-item>
-            </router-link>
-          </el-menu>
-        </el-aside>
+        <el-header class="header">
+          <el-dropdown v-if="store.isLogin">
+            <div>
+              <el-avatar
+                :size="44"
+                src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+              />
+            </div>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item @click="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          <router-link to="/login" v-else class="toLogin">登录</router-link>
+        </el-header>
         <el-main class="main">
           <router-view></router-view>
         </el-main>
@@ -59,34 +68,42 @@ const logout = () => {
 .header {
   display: flex;
   justify-content: space-between;
-  color: #fff;
-  background-color: #6699ff;
-  height: 80px;
-  line-height: 40px;
+  align-items: center;
+  background-color: #ffffff;
   padding: 20px 30px;
   overflow: hidden;
+}
+
+.header-left {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  height: 60px;
+  background-color: #0052D9;
+
+  .el-icon {
+    font-size: 24px;
+  }
 
   h2 {
     font-weight: 400;
-    font-size: 20px;
-    border-left: 1px solid #fff;
+    font-size: 15px;
     padding-left: 10px;
-  }
-  .toLogin {
-    color: #fff;
   }
 }
 
 .aside {
   width: 180px;
-
   .menu {
-    height: calc(100vh - 80px);
+    height: 100vh;
   }
 }
 
 .main {
   margin: 20px;
   background-color: #fff;
+  max-height: calc(100vh - 100px);
+  overflow: auto;
 }
 </style>
