8017c962245d859404c2e87a4734d5f9f9fa2cf0
查无此人 <woshi123qaq@163.com>
2025-07-01 14:35:15 +0800

    买家对接房屋列表

--- src/views/house/info/index.vue
+++ src/views/house/info/index.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="map-house-container">
-    <!-- 轮播图区域 -->
-    <div class="carousel-section">
-      <el-carousel :interval="4000" height="auto">
-        <el-carousel-item
-          v-for="(image, index) in carouselImages"
-          :key="index"
-          style="height: 600px;"
-        >
-          <img :src="image.url" :alt="image.alt" class="carousel-image" />
-        </el-carousel-item>
-      </el-carousel>
-    </div>
-
     <!-- 主要内容区域 -->
     <div class="main-content">
       <!-- 搜索面板 -->
@@ -61,7 +48,6 @@
         </el-form-item>
       </el-form>
 
-      <!-- 右侧房源卡片区域 -->
       <div class="house-cards-section">
         <div class="house-cards-grid">
           <div
@@ -86,16 +72,9 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { propertiesApi } from '@/api/index'
 
 const router = useRouter()
-
-// 轮播图数据
-const carouselImages = ref([
-  { url: 'https://pic4.zhimg.com/v2-3fad3b71721d643c918742fe2e75b943_1440w.jpg', alt: '轮播图1' },
-  { url: 'https://mofang-renter.oss-cn-shanghai.aliyuncs.com/store/1589887234819.jpg', alt: '轮播图2' },
-  { url: 'https://th.bing.com/th/id/R.afb27e3a0fcb84e7981a90a5a02f4800?rik=bSs9UafJVPDMgw&riu=http%3a%2f%2fszimg.xhj.com%2fxhj%2fimages%2f03dc38dd-7065-497f-bc56-39f93d340ce8.jpg&ehk=ge38AXyg9gOVeY2naXAqbcAwMd5nd2unVRJO76uCq4k%3d&risl=&pid=ImgRaw&r=0https://pic4.zhimg.com/v2-c5dd40c0226c91fb0eccae3f2d743563_r.jpg', alt: '轮播图3' },
-  { url: 'https://bjcache.leju.com/zxjiaju/zx_pic/20150109/db/60/d60b99acfed08692126b482eccb5621f.jpg', alt: '轮播图4' },
-])
 
 // 筛选条件
 const selectedAreas = ref([])
@@ -108,48 +87,20 @@ const houseObject = ref({
 })
 
 // 房源数据
-const houseList = ref([
-  {
-    id: 1,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.bfCdyDaBEjZEX_dMk9dk-QHaFj?w=238&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 2,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.xmdtYLyKyFJN-NakrqxcbwHaFj?w=238&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 3,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.WPL7bpQ6UGkp_cczqNUPkgHaFj?w=270&h=203&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 4,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.jI9oZoKQtbf1_Mg_BTFlTwHaFj?w=264&h=198&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 5,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.kjf6ebE9hmqxe30Tr3eRDgHaFi?w=260&h=194&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 6,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.wN4jVopu5875tDaoQlWlVAHaFj?w=261&h=196&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 7,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.PMZcVmTdIwkdEehM6HdWsQHaE8?w=294&h=196&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  },
-  {
-    id: 8,
-    name: '房源名称',
-    image: 'https://th.bing.com/th/id/OIP.lwVjCb_YXY9ALCkJ6jBr8QHaFj?w=243&h=182&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'
-  }
-])
+const houseList = ref([])
+
+const getHouseList = async () => {
+  const { data } = await propertiesApi.getAllProperties({
+    page: 1,
+    size: 1000000
+  })
+  houseList.value = data.map(item => ({
+    id: item.propertyid,
+    name: item.title,
+    image: item.image || 'https://www.dmoe.cc/random.php?id=' + Math.random()
+  }))
+}
+getHouseList();
 
 const onSubmit = () => {
   // 处理查询逻辑
